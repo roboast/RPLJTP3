271a861ed974fedf5813bd6ab8acb8a7e6a2c634
Asti Dhiya <astidhiyaanzaria@gmail.com>
2018-11-25 00:34:28 +0700

    update terbaru

---
+++ app/src/main/java/com/example/astidhiyaa/myapplication/activity_berita_guest.java
@@ -0,0 +1,66 @@
+package com.example.astidhiyaa.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class activity_berita_guest extends AppCompatActivity {
+    private DatabaseReference database;
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Post> dftrArtikel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita_guest);
+
+        rvView = (RecyclerView) findViewById(R.id.rv_berita_guest);
+        rvView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        rvView.setLayoutManager(layoutManager);
+        database = FirebaseDatabase.getInstance().getReference();
+
+
+        tampilData();
+
+    }
+    public void tampilData(){
+
+        database.child("artikel").child("id").orderByChild("tanggal").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                dftrArtikel = new ArrayList<>();
+                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
+
+                    Post post = noteDataSnapshot.getValue(Post.class);
+                    post.setId(noteDataSnapshot.getKey());
+
+
+                    dftrArtikel.add(post);
+                }
+
+                adapter = new AdapterBeritaGuest(dftrArtikel, activity_berita_guest.this);
+                rvView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println(databaseError.getDetails()+" "+databaseError.getMessage());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/astidhiyaa/myapplication/activity_wahana_guest.java
@@ -0,0 +1,69 @@
+package com.example.astidhiyaa.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+public class activity_wahana_guest extends AppCompatActivity {
+    private DatabaseReference database;
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Wahana> dftrWahana;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wahana_guest);
+
+        rvView = (RecyclerView) findViewById(R.id.rvNumbers);
+        int jlh_kolom = 2;
+        rvView.setLayoutManager(new GridLayoutManager(this,jlh_kolom));
+        database = FirebaseDatabase.getInstance().getReference();
+        storage = FirebaseStorage.getInstance();
+        tampilData();
+
+    }
+    public void tampilData(){
+
+        database.child("wahana").child("id").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                dftrWahana = new ArrayList<>();
+                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
+
+                    Wahana wahana = noteDataSnapshot.getValue(Wahana.class);
+                    wahana.setId(noteDataSnapshot.getKey());
+
+
+                    dftrWahana.add(wahana);
+                }
+
+                adapter = new AdapterWahanaGuest(dftrWahana, activity_wahana_guest.this);
+                rvView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println(databaseError.getDetails()+" "+databaseError.getMessage());
+            }
+        });
+    }
+}
